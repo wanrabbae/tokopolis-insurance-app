1634362731879497ffb9e91226bcfb243c775da2
Febriansyah <febriansyah602@gmail.com>
2022-11-29 14:18:10 +0700

    feat(role-management): grant permission endpoint to roles on file tokopolis.db

--- tokopolis_db.sql
+++ tokopolis_db.sql
@@ -380,6 +380,33 @@ CREATE TABLE `role_endpoints` (
   `endpoint_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `product_features`
+--
+
+INSERT INTO `role_endpoints` (`role_id`, `endpoint_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 10),
+(1, 11),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
+(1, 19),
+(1, 20),
+(1, 21);
+
 -- --------------------------------------------------------
 
 --
