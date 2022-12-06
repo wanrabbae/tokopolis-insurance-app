eb522fea0d217dd4fa799ab56fdcff914f7011b4
wanrabbae <wancode.msg@gmail.com>
2022-12-06 15:01:55 +0700

    fix(role-management): fix bug in verifyRole which cant id with string

--- core-admin/api/middlewares/verifyRole.js
+++ core-admin/api/middlewares/verifyRole.js
@@ -1,41 +1,50 @@
-import RoleService from '../services/RoleService'
+import RoleService from "../services/RoleService";
 
-const roleService = new RoleService()
-const jwt = require('jsonwebtoken')
+const roleService = new RoleService();
+const jwt = require("jsonwebtoken");
 
-module.exports = verify
+module.exports = verify;
 
-function verify(role = '') {
+function verify(role = "") {
     return [
         async (req, res, next) => {
-            const header = req.header('Authorization')
+            const header = req.header("Authorization");
 
             if (!header) {
-                return res.errorUnauthorized(req.polyglot.t('error.token.null'))
+                return res.errorUnauthorized(
+                    req.polyglot.t("error.token.null")
+                );
             }
 
-            const token = header.split(' ')
+            const token = header.split(" ");
 
             try {
-                const originEndpoint = String(req.baseUrl + req.path)
-                const endpoint = originEndpoint.replace(/[0-9]/g, "?")
-                const method = req.method
-                
-                const verified = jwt.verify(token[1], process.env.TOKEN_SECRET)
-                req.account = verified
+                const originEndpoint = String(req.baseUrl + req.path);
+                const endpoint = originEndpoint
+                    .replace(/([A-Z][A-Z][A-Z]-\d+-+\d+-\d)\w+/g, "?")
+                    .replace(/[0-9]/g, "?");
+                const method = req.method;
 
-                if (typeof role === 'string' && role == 'auth:role') {
-                    const endpoints = await roleService.getAllEndpointExist(req.account.role, endpoint, method)
+                const verified = jwt.verify(token[1], process.env.TOKEN_SECRET);
+                req.account = verified;
+
+                if (typeof role === "string" && role == "auth:role") {
+                    const endpoints = await roleService.getAllEndpointExist(
+                        req.account.role,
+                        endpoint,
+                        method
+                    );
                     if (endpoints.length == 0) {
-                        return res.errorUnauthorized(req.polyglot.t('error.token.role'))
+                        return res.errorUnauthorized(
+                            req.polyglot.t("error.token.role")
+                        );
                     }
                 }
-                
-                return next()
-            } catch (err) {
-                return res.errorUnauthorized(req.polyglot.t('error.token'))
-            }
-        }
-    ]
-}
 
+                return next();
+            } catch (err) {
+                return res.errorUnauthorized(req.polyglot.t("error.token"));
+            }
+        },
+    ];
+}
