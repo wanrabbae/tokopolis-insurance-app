9d112d993aee291abfbbeb047bad77e5be99c82b
wanrabbae <wancode.msg@gmail.com>
2022-12-06 09:59:09 +0700

    feat(role-management): add role tree

--- core-admin/api/repositories/RoleRepository.js
+++ core-admin/api/repositories/RoleRepository.js
@@ -1,87 +1,103 @@
-const { QueryTypes, Op } = require('sequelize')
+const { QueryTypes, Op } = require("sequelize");
 
-const { sequelize, Role, RoleEndpoint } = require('../models')
+const { sequelize, Role, RoleEndpoint, Account } = require("../models");
 
 export default class RoleRepository {
-  constructor() {}
+    constructor() {}
 
-  async getRoleName(name) {
-    return await Role.findOne({ where: { name: name } })
-  }
+    async getRoleName(name) {
+        return await Role.findOne({ where: { name: name } });
+    }
 
-  async createRole(payload) {
-    return await Role.create(payload)
-  }
+    async createRole(payload) {
+        return await Role.create(payload);
+    }
 
-  async getRoleAllWithFilter(query, limit, offset) {
-    return await Role.findAll({
-      include: {
-        model: RoleEndpoint,
-        as: 'role_endpoints'
-      },
-      where: {
-        [Op.or]: [
-          { name: { [Op.like]: `%${query}%` } },
-        ]
-      },
-      limit: limit,
-      offset: offset,
-    })
-  }
+    async getRoleAllWithFilter(query, limit, offset) {
+        return await Role.findAll({
+            include: {
+                model: RoleEndpoint,
+                as: "role_endpoints",
+            },
+            where: {
+                [Op.or]: [{ name: { [Op.like]: `%${query}%` } }],
+            },
+            limit: limit,
+            offset: offset,
+        });
+    }
 
-  async getCountAll() {
-    return await Role.count()
-  }
+    async getRoleTree(role_id) {
+        return await Role.findAll({
+            where: {
+                id: {
+                    [Op.lte]: role_id,
+                },
+            },
+            include: {
+                model: Account,
+                as: "accounts",
+                attributes: ["id", "fullname", "email"],
+            },
+        });
+    }
 
-  async getCountByQuery(query) {
-    return await Role.count({
-      where: {
-        [Op.or]: [
-          { name: { [Op.like]: `%${query}%` } },
-        ]
-      },
-    })
-  }
+    async getCountAll() {
+        return await Role.count();
+    }
 
-  async getRoleById(id) {
-    return await Role.findByPk(id)
-  }
+    async getCountByQuery(query) {
+        return await Role.count({
+            where: {
+                [Op.or]: [{ name: { [Op.like]: `%${query}%` } }],
+            },
+        });
+    }
 
-  async update(id,payload) {
-    return await Role .update(payload, {
-      where: { id: id }
-    })
-  }
+    async getRoleById(id) {
+        return await Role.findByPk(id);
+    }
 
-  async destroy(id) {
-    return await Role .destroy({
-      where: { id: id }
-    })
-  }
+    async update(id, payload) {
+        return await Role.update(payload, {
+            where: { id: id },
+        });
+    }
 
-  async addEndpoint(payload) {
-    return await RoleEndpoint.create(payload)
-  }
+    async destroy(id) {
+        return await Role.destroy({
+            where: { id: id },
+        });
+    }
 
-  async removeEndpoint(id) {
-    return await RoleEndpoint.destroy({
-      where: { id: id }
-    })
-  }
+    async addEndpoint(payload) {
+        return await RoleEndpoint.create(payload);
+    }
 
-  async endpointExist(payload) {
-    return await RoleEndpoint.findOne({
-      where: { role_id: payload.role_id, endpoint_id: payload.endpoint_id }
-    })
-  }
+    async removeEndpoint(id) {
+        return await RoleEndpoint.destroy({
+            where: { id: id },
+        });
+    }
 
-  async getAllEndpointExist(role_id, endpoint, method) {
-    return await sequelize.query(`SELECT roles.id, endpoints.route, endpoints.method FROM roles ` +
-    `JOIN role_endpoints ON role_endpoints.role_id = roles.id ` +
-    `JOIN endpoints ON endpoints.id = role_endpoints.endpoint_id ` +
-    `WHERE roles.id = '${role_id}' ` +
-    `AND endpoints.route = '${endpoint}' ` +
-    `AND endpoints.method = '${method}' `,
-    { type: QueryTypes.SELECT })
-  }
+    async endpointExist(payload) {
+        return await RoleEndpoint.findOne({
+            where: {
+                role_id: payload.role_id,
+                endpoint_id: payload.endpoint_id,
+            },
+        });
+    }
+
+    async getAllEndpointExist(role_id, endpoint, method) {
+        return await sequelize.query(
+            `SELECT roles.id, endpoints.route, endpoints.method FROM roles ` +
+                `JOIN role_endpoints ON role_endpoints.role_id = roles.id ` +
+                `JOIN endpoints ON endpoints.id = role_endpoints.endpoint_id ` +
+                `WHERE roles.id = '${role_id}' ` +
+                `AND endpoints.route = '${endpoint}' ` +
+                `AND endpoints.method = '${method}' `,
+            { type: QueryTypes.SELECT }
+        );
+    }
 }
--- core-admin/api/routes/admin/role.routes.js
+++ core-admin/api/routes/admin/role.routes.js
@@ -1,18 +1,27 @@
-const { Router } = require('express')
-const verifyToken = require('../../middlewares/verifyRole')
+const { Router } = require("express");
+const verifyToken = require("../../middlewares/verifyRole");
 
-const { create, list, update, destroy, addEndpoint, removeEndpoint } = require('../../controllers/admin/RoleController')
+const {
+    create,
+    list,
+    update,
+    destroy,
+    addEndpoint,
+    removeEndpoint,
+    roleTree,
+} = require("../../controllers/admin/RoleController");
 
-const router = Router()
-const AuthRoleMiddleware = verifyToken('auth:role')
+const router = Router();
+const AuthRoleMiddleware = verifyToken("auth:role");
 
-router.post('/admin/role', AuthRoleMiddleware, create)
-router.get('/admin/role/list',AuthRoleMiddleware,list)
-router.put('/admin/role/:id', AuthRoleMiddleware, update)
-router.delete('/admin/role/:id', AuthRoleMiddleware, destroy)
+router.post("/admin/role", AuthRoleMiddleware, create);
+router.get("/admin/role/list", AuthRoleMiddleware, list);
+router.get("/admin/role/tree", AuthRoleMiddleware, roleTree);
+router.put("/admin/role/:id", AuthRoleMiddleware, update);
+router.delete("/admin/role/:id", AuthRoleMiddleware, destroy);
 
 // Role Management
-router.post('/admin/role/endpoint/add', AuthRoleMiddleware, addEndpoint)
-router.delete('/admin/role/endpoint/:id', AuthRoleMiddleware, removeEndpoint)
+router.post("/admin/role/endpoint/add", AuthRoleMiddleware, addEndpoint);
+router.delete("/admin/role/endpoint/:id", AuthRoleMiddleware, removeEndpoint);
 
-module.exports = router
+module.exports = router;
