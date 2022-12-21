0e11d27fd1d2c24d535c20ababe5b9f19708595d
Rahmat Ansori <32799849+ansori34@users.noreply.github.com>
2022-12-22 05:50:04 +0700

    ENH: [SERVER] Update Project Auto Setup (#110)
    - Update setup.sh
    - Update Environment Variables
    - Fix core-client Nuxt versio

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@ MYSQL_USER="anshor"
 MYSQL_PASS="vpsdb"
 MYSQL_DB="db_tokopolis"
 
+# Install Node JS and PM2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.profile
+nvm install 14.18.3
+npm install -g pm2
+
 # Config MySQL
 mysql -u root << EOFMYSQL
 
@@ -19,6 +25,35 @@ cp .env.client core-client/.env
 cp .env.email email-service/.env
 cp .env.payment payment-service/.env
 
+# Config Nginx
+cp nginx-admin.conf /etc/nginx/sites-available/tokopolis-admin.conf
+ln -sf /etc/nginx/sites-available/tokopolis-admin.conf /etc/nginx/sites-enabled/tokopolis-admin.conf
+
+cp nginx-client.conf /etc/nginx/sites-available/tokopolis-client.conf
+ln -sf /etc/nginx/sites-available/tokopolis-client.conf /etc/nginx/sites-enabled/tokopolis-client.conf
+
+nginx -t
+systemctl restart nginx
+
+# Config Golang
+apt-get purge golang*
+
+curl -O https://dl.google.com/go/go1.18.4.linux-amd64.tar.gz
+tar xvf go1.18.4.linux-amd64.tar.gz
+mv go /usr/local
+
+mkdir ~/.go
+
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOPATH=~/.go" >> ~/.profile
+echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.profile
+source ~/.profile
+
+update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0
+update-alternatives --set go /usr/local/go/bin/go
+
+# ============================================================================
+
 # Config Admin
 cd core-admin/
 npm run packaging
@@ -34,14 +69,9 @@ pm2 start npm --name "tokopolis-client" -- start
 # Config Payment
 cd ../payment-service/
 npm run packaging
-pm2 start npm --name "tokopolis-payment" -- dev
+pm2 start npm --name "tokopolis-payment" -- run dev
 
-# Config Nginx
-cp nginx-admin.conf /etc/nginx/sites-available/tokopolis-admin.conf
-ln -sf /etc/nginx/sites-available/tokopolis-admin.conf /etc/nginx/sites-enabled/tokopolis-admin.conf
-
-cp nginx-client.conf /etc/nginx/sites-available/tokopolis-client.conf
-ln -sf /etc/nginx/sites-available/tokopolis-client.conf /etc/nginx/sites-enabled/tokopolis-client.conf
-
-nginx -t
-systemctl restart nginx
+# Config Email
+cd ../email-service/
+go build main.go
+pm2 start ./main --name "tokopolis-email"
