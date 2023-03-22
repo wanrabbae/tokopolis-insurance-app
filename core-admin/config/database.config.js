dd1063ac4accdb9702645d1bd83d858db97236dc
wanrabbae <wancode.msg@gmail.com>
2023-03-22 13:05:21 +0700

    Change db port

--- core-admin/config/database.config.js
+++ core-admin/config/database.config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: 8889,
+    port: 3306,
     dialect: 'mysql',
   },
   test: {
