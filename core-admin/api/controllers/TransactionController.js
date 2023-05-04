1de51b33f0a432ce77b400209123584375bb8732
Putra Irawan <31697177+putrairawan992@users.noreply.github.com>
2023-05-04 20:17:35 +0700

    Fix/present 1 (#258)
    * FEAT: [BACKEND] Daftar produk (email) dapat banyak cc email
    
    * Feat: [BACKEND] Penggunacrud api|[BACKEND] Management Klaim (Tampilkan Daftar Klaim, Update Status Klaim, generate file excel & zip, kirim
    
    * Feat: [BACKEND] Feat transaction feedback agent & Revise transaction
    
    * Feat: [BACKEND] Feat transaction feedback agent & Revise transaction
    
    * Feat: [BACKEND] Tarik Komisi & Poin (untuk role spv, branch head, OM)
    
    * ENH: [BACKEND] Fix withdraw and reduce the total
    
    * FIX: FE|BE feedback session 1
    
    * FIX: [BE][FE] Update callback api and generate
    
    * FIX: [BE][FE] Update callback api and generate
    
    * FIX: [BE][FE] Update callback api and generate
    
    * FIX: [BE][FE] Update feedback review 2
    
    * ENH: [FE][BE] Feedback review BE
    
    * ENH: [FE][BE] fixed feedback review
    
    * ENH: [FE][BE] fixed feedback review
    
    * FIX: [FE] update email column
    
    * ENH: [BE][FE] Feedback and Enhance Task
    
    * feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH: [BE] update fixed query
    
    * ENH: [BE] update fixed query
    
    * ENH: [BE] update fixed query
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    * ENH:[FE][BE] feedback-present
    
    ---------
    
    Co-authored-by: putrairawan992 <108646036+putrairawanfazz@users.noreply.github.com>

--- core-admin/api/controllers/TransactionController.js
+++ core-admin/api/controllers/TransactionController.js
@@ -812,6 +812,11 @@ exports.doPayment = async (req, res) => {
         transaction.total
     );
 
+
+    if (typeof transaction.client_data == "string") {
+        transaction.client_data = transaction.client_data
+    }
+
     const payload = {
         order_id: transaction.id,
         customer: {
